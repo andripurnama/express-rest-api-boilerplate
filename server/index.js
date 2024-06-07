e01fd12c019f1352d248f130c7a60cb9f6af9b69
andripurnama <andri.purnama995@hotmail.com>
2024-06-07 16:59:52 +0700

    refactor: call function for bootstrap app

--- server/index.js
+++ server/index.js
@@ -1,5 +1,2 @@
-import app from '../app';
-import dotenv from "dotenv";
-dotenv.config()
-const PORT = process.env.APP_PORT || 8888;
-app.listen(PORT, () => console.log(`Server is connected on ${PORT}`));
+import bootstrapApp from '../src';
+bootstrapApp();
