b7e83e0b4d7ad7226778086a5fafd6cdd79dd13d
andripurnama <andri.purnama995@hotmail.com>
2024-06-07 15:01:40 +0700

    test: added user repository test

---
+++ tests/units/user.repository.test.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config()
+describe('user repository tests', () => {
+    it('should user repository class exists', () => {
+        const userRepository = new UserRepository();
+        
+    })
+})
