3c2da355d971b9589a243b91bcb0aa7aaad0a103
andripurnama <andri.purnama995@hotmail.com>
2024-06-11 15:27:45 +0700

    refactor: added database initiate in bootstrap app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import express from 'express'
+import 'dotenv/config';
+import express from 'express';
 import { routes } from './routes';
+import db from '@/models';
+async function bootstrapApp() {
+  const app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  const PORT = process.env.APP_PORT || 3000;
+  db.sequelize
+    .sync()
+    .then(() => {
+      app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+      });
+    })
+    .catch((error) => {
+      console.error('Unable to connect to the database:', error);
+    });
+}
 
-const app = express();
-
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
-
-
-export default app;
+export default bootstrapApp;
