07e86f3668aca50b926b9c2ee89b7bf3c4296d17
andripurnama <andri.purnama995@hotmail.com>
2024-06-11 15:31:34 +0700

    feat: added user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,18 +1,20 @@
 import { Model, DataTypes } from 'sequelize';
-import sequelize from './index';
-
+import sequelize from '../config/sequelize/sequelize';
 class User extends Model {}
-User.init({
-  firstName: DataTypes.STRING,
-  lastName: DataTypes.STRING,
-  email: DataTypes.STRING,
-  password: DataTypes.STRING,
-  isActive: DataTypes.BOOLEAN,
-  createdAt: DataTypes.DATE,
-  updatedAt: DataTypes.DATE
-}, {
-  sequelize,
-  modelName: 'User',
-});
+User.init(
+  {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    isActive: DataTypes.BOOLEAN,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  },
+  {
+    sequelize,
+    modelName: 'User',
+  }
+);
 
 export default User;
