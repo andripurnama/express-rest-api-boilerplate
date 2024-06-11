9cfd30155b900f1c9f55ef85f8a0ba88b3154bd6
andripurnama <andri.purnama995@hotmail.com>
2024-06-11 15:32:22 +0700

    feat: added mapping model files

--- src/models/index.js
+++ src/models/index.js
@@ -1,32 +1,30 @@
-import { Sequelize } from 'sequelize';
-import config from '../config/database';
 import fs from 'fs';
 import path from 'path';
+import sequelize from '../config/sequelize/sequelize';
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const dbConfig = config[env];
-
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
-
 const db = {};
 
+// Dynamically import all models
 fs.readdirSync(__dirname)
-  .filter(file => {
-    return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
+  .filter((file) => {
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file)).default(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file)).default;
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+// Call associate method if defined
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.Sequelize = require('sequelize');
 
 export default db;
